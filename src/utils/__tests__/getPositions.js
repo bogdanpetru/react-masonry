78178697d539b23deb38f5b46a5dce3e26fc3292
bogdanpetru <bogdanpintican@gmail.com>
2017-02-22 00:22:48 +0200

    Add algorithm part to arange first row when all boxes fit

--- src/utils/__tests__/getPositions.js
+++ src/utils/__tests__/getPositions.js
@@ -1,14 +1,31 @@
 import getPositions from '../getPositions'
 
 describe('getPositions', () => {
-  test('should return correct positions on two rows', () => {
+  test('should position first row correct', () => {
+    const boxes = [
+      { width: 30, height: 30 },
+      { width: 25, height: 25 },
+      { width: 25, height: 25 },
+      { width: 20, height: 20 }
+    ]
+    const width = 100
+    const expected = [
+      { top: 0, left: 0 },
+      { top: 0, left: 30 },
+      { top: 0, left: 55 },
+      { top: 0, left: 80 }
+    ]
+    expect(getPositions({ boxes, width }))
+      .toEqual(expected)
+  })
+  xtest('should return correct positions on two rows', () => {
     const boxes = [
       { width: 3, height: 3 }, // 1
       { width: 4, height: 2 }, // 2
       { width: 4, height: 4 }, // 3
       { width: 5, height: 2 }, // 4
       { width: 4, height: 3 }, // 5
-        { width: 5, height: 1 }, // 6
+      { width: 5, height: 1 }, // 6
       { width: 3, height: 3 }, // 7
       { width: 4, height: 2 }, // 8
       { width: 3, height: 2 }, // 9
--- src/utils/getPositions.js
+++ src/utils/getPositions.js
@@ -5,10 +5,22 @@ type box = {
   height: number,
 }
 
-type boxes = box[]
+function getPositions({ boxes, width } : { boxes: box[], width: number}) {
+  // position first row
+  const intermediarPositions = boxes.reduce(
+    (acc, item) => {
+      acc.positions.push({
+        top: acc.currentTop,
+        left: acc.currentLeft
+      })
+      acc.currentLeft += item.width
 
-function getPositions({ boxes, width } : { boxes: boxes, width: number}) {
+      return acc
+    },
+    { currentLeft: 0, currentTop: 0, positions: [] }
+  )
 
+  return intermediarPositions.positions
 }
 
 export default getPositions
