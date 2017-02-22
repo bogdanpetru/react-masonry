1613521e49fe56c439728a407fcb796b48ff10b8
bogdanpetru <bogdanpintican@gmail.com>
2017-02-23 00:03:22 +0200

    Add test case for 2 rows

--- src/utils/__tests__/getPositions.js
+++ src/utils/__tests__/getPositions.js
@@ -1,23 +1,54 @@
-import getPositions from '../getPositions'
+import getPositions from '../getPositions';
 
 describe('getPositions', () => {
-  test('should position first row correct', () => {
-    const boxes = [
-      { width: 30, height: 30 },
-      { width: 25, height: 25 },
-      { width: 25, height: 25 },
-      { width: 20, height: 20 }
-    ]
-    const width = 100
-    const expected = [
-      { top: 0, left: 0 },
-      { top: 0, left: 30 },
-      { top: 0, left: 55 },
-      { top: 0, left: 80 }
-    ]
-    expect(getPositions({ boxes, width }))
-      .toEqual(expected)
-  })
+  describe('optimal sizes, all of them fit in the row', () => {
+    test('should position first row correct', () => {
+      const boxes = [
+        { width: 30, height: 30 },
+        { width: 25, height: 25 },
+        { width: 25, height: 25 },
+        { width: 20, height: 20 },
+      ];
+      const width = 100;
+      const expected = [
+        { top: 0, left: 0 },
+        { top: 0, left: 30 },
+        { top: 0, left: 55 },
+        { top: 0, left: 80 },
+      ];
+      expect(getPositions({ boxes, width })).toEqual(expected);
+    });
+    test('should position second row correct', () => {
+      const boxes = [
+        { width: 30, height: 30 }, // last
+        { width: 25, height: 25 }, // 2nd
+        { width: 25, height: 25 }, // 3rd
+        { width: 20, height: 20 }, // first position
+
+        // second row
+        { width: 25, height: 30 }, // goes in 2 because it doesn't fit in fisrt
+        { width: 20, height: 25 }, // goes in (4) first position
+        { width: 25, height: 25 }, // goes in 3
+        { width: 20, height: 20 }, // goes in (1) in last position
+      ];
+
+      const expected = [
+        { top: 0, left: 0 },
+        { top: 0, left: 30 },
+        { top: 0, left: 55 },
+        { top: 0, left: 80 },
+
+        // second row
+        { top: 24, left: 30 },
+        { top: 20, left: 80 },
+        { top: 25, left: 55 },
+        { top: 30, left: 0 },
+      ];
+      const width = 15;
+      expect(getPositions({ boxes, width })).toEqual(expected);
+    });
+  });
+
   xtest('should return correct positions on two rows', () => {
     const boxes = [
       { width: 3, height: 3 }, // 1
@@ -29,7 +60,7 @@ describe('getPositions', () => {
       { width: 3, height: 3 }, // 7
       { width: 4, height: 2 }, // 8
       { width: 3, height: 2 }, // 9
-    ]
+    ];
     const expected = [
       { left: 0, top: 0 }, // 1
       { left: 3, top: 0 }, // 2
@@ -40,9 +71,8 @@ describe('getPositions', () => {
       { left: 0, top: 3 }, // 7
       { left: 10, top: 3 }, // 8
       { left: 7, top: 5 }, // 9
-    ]
-    const width = 15
-    expect(getPositions({ boxes, width }))
-      .toEqual(expected)
-  })
-})
+    ];
+    const width = 15;
+    expect(getPositions({ boxes, width })).toEqual(expected);
+  });
+});
--- src/utils/getPositions.js
+++ src/utils/getPositions.js
@@ -3,24 +3,29 @@
 type box = {
   width: number,
   height: number,
-}
+};
 
-function getPositions({ boxes, width } : { boxes: box[], width: number}) {
+function getPositions({ boxes, width }: { boxes: box[], width: number }) {
   // position first row
   const intermediarPositions = boxes.reduce(
     (acc, item) => {
+      // find out when a new row starts
+      if (acc.currentLeft + item.width > width) {
+        // new row
+      }
+
       acc.positions.push({
         top: acc.currentTop,
-        left: acc.currentLeft
-      })
-      acc.currentLeft += item.width
+        left: acc.currentLeft,
+      });
+      acc.currentLeft += item.width;
 
-      return acc
+      return acc;
     },
-    { currentLeft: 0, currentTop: 0, positions: [] }
-  )
+    { currentLeft: 0, currentTop: 0, positions: [] },
+  );
 
-  return intermediarPositions.positions
+  return intermediarPositions.positions;
 }
 
-export default getPositions
+export default getPositions;
