bd1747715cc835937cb07e91c9ee58cf66abeb30
bogdanpetru <bogdanpintican@gmail.com>
2017-03-14 23:41:56 +0200

    Add test for 2nd stone

--- src/utils/__tests__/placeStone.js
+++ src/utils/__tests__/placeStone.js
@@ -18,4 +18,26 @@ describe('placeStone', () => {
       })
     ).toEqual(expected)
   })
+  it('should place the 2nd stone correct', () => {
+    const stone = { width: 30, height: 30 }
+    const availableSpots = [
+      { top: 0, left: 50, right: 100, bottom: null },
+      { top: 50, left: 0, right: 100, bottom: null }
+    ]
+    const expected = {
+      position: { top: 0, left: 50 },
+      availableSpots: [
+        { top: 0, left: 80, right: 100, bottom: null },
+        { top: 50, left: 0, right: 100, bottom: null },
+        { top: 30, left: 50, right: 100, bottom: null },
+      ]
+    }
+    const containerSize = 100
+    expect(
+      placeStone({
+        stone, availableSpots, containerSize
+      })
+    ).toEqual(expected)
+
+  })
 })
