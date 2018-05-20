67a718c771f5101b9d506c7b926f6ffff46be143
b <bogdanpintican@gmail.com>
2018-05-20 22:13:20 +0300

    Extract spot validation into more refined functions

--- src/utils/validateSpots.js
+++ src/utils/validateSpots.js
@@ -1,99 +1,116 @@
+const isSpotConsumed = spot =>
+  // width
+  spot.right - spot.left < 5 ||
+  // height
+  (spot.bottom && spot.bottom - spot.top < 5);
+
+// constrained spot must not overlap with an existing spot
+// this happens when is constrained and another includes it
+/*
+  +--------------------------------------+
+  |                   +------------------|
+  |                   |                 ||
+  |                   |                 ||
+  |                   |                 ||
+  |                   +------------------|
+  |                   |                 ||
+  |                   |                 ||
+  |                   |   should be removed
+  |                   |                 ||
+  |                   |                 ||
+  |                   |                 ||
+  |                   +------------------|
+  +--------------------------------------+
+*/
+const isSpotContained = ({ spotsList, usedSpot }) =>
+  spotsList.some(
+    currentSpot =>
+      currentSpot !== usedSpot && usedSpot.left === currentSpot.left
+  );
+
+const constrainRight = ({ position, spot, stone }) => {
+  // if spot is above and restricts it's right
+  if (
+    position.left > spot.left && // make sure it is to the left
+    spot.right >= position.left && // and right is already smaller than this position right
+    position.top + stone.height > spot.top // make sure it is above it
+  ) {
+    // if it has a bottom, make sure it interescts it
+    if (spot.bottom && spot.bottom < position.top) {
+      return spot;
+    }
+
+    const constrainedSpot = { ...spot };
+    constrainedSpot.right = position.left;
+    return constrainedSpot;
+  }
+
+  return null;
+};
+
+const constrainBottom = ({ position, stone, spot }) => {
+  // check if it was placed above this spot
+  if (position.left + stone.width > spot.left && position.top >= spot.top) {
+    // if it has already a bottom, must check if it really below it
+    // and inside it's left/right
+    if (spot.bottom && spot.bottom < position.top) {
+      return spot;
+    }
+
+    // make sure used space actually intersect
+    if (spot.right < position.left) {
+      return spot;
+    }
+
+    const constrainedSpot = { ...spot };
+    constrainedSpot.bottom = position.top;
+    return constrainedSpot;
+  }
+
+  return null;
+}
+
 export function validateSpots({
   spots,
   position,
   optimalSpot,
-  stone,
+  stone
 }: {
   spots: Spot[],
   position: Position,
   optimalSpot: Spot,
   stone: Stone
 }) {
- return spots.map((spot: Spot, index, spotsList) => {
+  return spots.map((spot: Spot, index, spotsList) => {
     if (spot === optimalSpot) {
       // restrict used spot
       let usedSpot = { ...optimalSpot };
       usedSpot.left = position.left + stone.width;
 
-      // if the spot is consumed it should be removed
-      if (usedSpot.right - usedSpot.left < 5) {
+      if (isSpotConsumed(usedSpot)) {
         return null;
       }
 
-      // if it has height it should be bigger than 5
-      if (usedSpot.bottom && usedSpot.bottom - usedSpot.top < 5) {
-        return null;
-      }
-
-      // constrained spot must not overlap with an existing spot
-      // this happens when is constrained and another includes it
-      /*
-        +--------------------------------------+
-       |                   +------------------|
-       |                   |                 ||
-       |                   |                 ||
-       |                   |                 ||
-       |                   +------------------|
-       |                   |                 ||
-       |                   |                 ||
-       |                   |   should be removed
-       |                   |                 ||
-       |                   |                 ||
-       |                   |                 ||
-       |                   +------------------|
-       +--------------------------------------+
-       
-      */
-      const spotIsContained = spotsList.some(
-            (spot, innerIndex) => innerIndex !== index && usedSpot.left === spot.left
-      );
-      if (spotIsContained) {
+      if (isSpotContained({ usedSpot, spotsList })) {
         return null;
       }
 
       return usedSpot;
     }
 
-    // if spot is above and restricts it's right
-    if (
-      position.left > spot.left  // make sure it is to the left
-      && spot.right >= position.left  // and right is already smaller than this position right
-      && position.top + stone.height > spot.top // make sure it is above it
-    ) {
-
-      // if it has a bottom, make sure it interescts it
-      if (spot.bottom && spot.bottom < position.top) {
-        return spot;
-      }
-
-      const constrainedSpot = { ...spot };
-      constrainedSpot.right = position.left;
-      return constrainedSpot;
+    // check if it used this spot also
+    if (spot.top === position.top) {
+      return null;
     }
 
-   // check if it was placed above this spot
-    if (
-      position.left + stone.width > spot.left && position.top >= spot.top
-    ) {
-      // check if it used this spot also
-      if (spot.top === position.top) {
-        return null;
-      }
+    const rightConstrained = constrainRight({position, spot, stone});
+    if (rightConstrained) {
+      return rightConstrained;
+    }
 
-      // if it has already a bottom, must check if it really below it
-      // and inside it's left/right
-      if (spot.bottom && spot.bottom < position.top) {
-        return spot;
-      }
-
-      // make sure used space actually intersect
-      if (spot.right < position.left) {
-        return spot;
-      }
-      
-      const constrainedSpot = { ...spot };
-      constrainedSpot.bottom = position.top;
-      return constrainedSpot;
+    const bottomConstrained = constrainBottom({position, stone, spot});
+    if (bottomConstrained) {
+      return bottomConstrained;
     }
 
     return spot;
