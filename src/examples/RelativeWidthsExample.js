112a02acb2ce1c4cbb1c75d6bdd07ca02c49a77b
Bogdan Petru Pintican <bogdan.pintican@8x8.com>
2019-01-04 22:23:53 +0200

    Add stacking draft

--- src/examples/RelativeWidthsExample.js
+++ src/examples/RelativeWidthsExample.js
@@ -45,10 +45,12 @@ export function randomColor() {
   return `rgb(${random250()},${random(0, 100)},${random(0, 120)})`;
 }
 
+const colors = boxes.map(() => randomColor());
+
 const App = ({ stacking }) => (
   <Masonry style={{ height: 500 }} stacking={stacking} transition="fadeMove">
     {boxes.map((box, index) => (
-      <div key={index} style={{ ...box, backgroundColor: randomColor() }}>
+      <div key={index} style={{ ...box, backgroundColor: colors[index]}}>
         {`${box.width} - ${box.height}`}
         <div style={titleStyle}>{index}</div>
       </div>
