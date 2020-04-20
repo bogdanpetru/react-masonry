64ba570def46a111c6f25a055e44038eb968a37e
bogdan petru pintican <bogdan.pintican@8x8.com>
2020-04-20 21:40:41 +0300

    Updates

---
+++ precache-manifest.ab7786378e1d4b9be55a94f9c1764032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4787b7052a2f72ee31ae22d373a5e46",
+    "url": "/react-masonry/index.html"
+  },
+  {
+    "revision": "2ecd9a32391b6dcfa5a9",
+    "url": "/react-masonry/static/css/main.8a38e5ca.chunk.css"
+  },
+  {
+    "revision": "e95e6347dc20ea7a3410",
+    "url": "/react-masonry/static/js/2.bead0e91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-masonry/static/js/2.bead0e91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ecd9a32391b6dcfa5a9",
+    "url": "/react-masonry/static/js/main.1df3a88a.chunk.js"
+  },
+  {
+    "revision": "33a5fe71aaed96193692",
+    "url": "/react-masonry/static/js/runtime-main.92cc9ddd.js"
+  }
+]);
