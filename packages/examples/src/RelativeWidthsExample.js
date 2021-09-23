14336d9e3626ecfcc4893fd03f684d25a91362e5
Bogdan Petru Pintican <bogdanpintican@gmail.com>
2021-09-23 13:47:04 +0300

    upgrade to yarn workspaces

--- packages/examples/src/RelativeWidthsExample.js
+++ packages/examples/src/RelativeWidthsExample.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { Masonry } from "../lib";
+import React from 'react';
+import { Masonry } from 'react-masonry';
 
 const common = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  fontWeight: 700
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontWeight: 700,
 };
 
 const titleStyle = {
-  position: "absolute",
+  position: 'absolute',
   left: 5,
   top: 5,
-  fontWeight: "700",
-  backgroundColor: "#333",
-  color: "#fff",
-  padding: 5
+  fontWeight: '700',
+  backgroundColor: '#333',
+  color: '#fff',
+  padding: 5,
 };
 
 function random(min, max) {
@@ -30,7 +30,7 @@ function getBox() {
   return {
     ...common,
     height: random(160, 640),
-    backgroundColor: randomColor()
+    backgroundColor: randomColor(),
   };
 }
 
@@ -38,13 +38,15 @@ function randomColor() {
   return `rgb(${random250()},${random(0, 100)},${random(0, 120)})`;
 }
 
-function getImageSrc(height){
-  return [
-    'https://loremflickr.com/230/',
-    'https://www.fillmurray.com/230/',
-    'https://placebeard.it/230x',
-    'https://www.placecage.com/230/'
-  ][random(0, 2)] + height;
+function getImageSrc(height) {
+  return (
+    [
+      'https://loremflickr.com/230/',
+      'https://www.fillmurray.com/230/',
+      'https://placebeard.it/230x',
+      'https://www.placecage.com/230/',
+    ][random(0, 2)] + height
+  );
 }
 
 const App = ({ stacking, numberOfBoxes = 1 }) => {
