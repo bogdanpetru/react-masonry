f48a8f21fb10db11d66ccdd4c7ab1f3171dfe407
Bogdan Petru Pintcan <bogdanpintican@gmail.com>
2022-02-15 19:43:34 +0200

    progress on styling

--- packages/site/src/App.jsx
+++ packages/site/src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Home } from './views/home'
-import { Api } from './views/api'
+import { Home } from './views/Home'
+import { Api } from './views/Api'
+import { Examples } from './views/Examples'
+import { Header } from './components/Header'
 
 import './App.css'
 import { Navigation } from './components/Navigation'
@@ -9,15 +11,12 @@ import { Navigation } from './components/Navigation'
 const App = () => {
   return (
     <BrowserRouter>
-      <main>
-        <h1 className="title">React Masonry</h1>
-        <Navigation />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/api" element={<Api />} />
-        </Routes>
-      </main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/api" element={<Api />} />
+        <Route path="/examples" element={<Examples />} />
+      </Routes>
     </BrowserRouter>
   )
 }
--- packages/site/src/views/Home.jsx
+++ packages/site/src/views/Home.jsx
@@ -1,30 +1,4 @@
-import React, { useState } from 'react'
-import RelativeWidthsExample from './examples/RelativeWidthsExample'
-
-const options = [
-  {
-    id: 'right-bottom',
-    label: 'right, bottom',
-  },
-  {
-    id: 'left-bottom',
-    label: 'left, bottom',
-  },
-  {
-    id: 'right-top',
-    label: 'right, top',
-  },
-  {
-    id: 'left-top',
-    label: 'left, top',
-  },
-]
-
 export const Home = () => {
-  const [numberOfBoxes, setCardsNumber] = useState(30)
-  const [gutter, setGutter] = useState(0)
-  const [stacking, setStacking] = useState(3)
-
   return (
     <div className="root">
       <main>
@@ -79,28 +53,7 @@ export const Home = () => {
           </a>
           .
         </p>
-        <h2>Examples</h2>
-        <select onChange={(event) => setStacking(event.target.value)}>
-          {options.map((item, key) => (
-            <option value={item.id}>{item.label}</option>
-          ))}
-        </select>
-        <input
-          type="number"
-          value={numberOfBoxes}
-          onChange={(event) => setCardsNumber(event.target.value)}
-        />
-        <input
-          type="number"
-          value={gutter}
-          onChange={(event) => setGutter(event.target.value)}
-        />
       </main>
-      <RelativeWidthsExample
-        stacking={stacking}
-        gutter={gutter}
-        numberOfBoxes={numberOfBoxes}
-      />
     </div>
   )
 }
