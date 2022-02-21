a1ff085fc8929868695d6cf94982778988af641e
Bogdan Petru Pintican <bogdanpintican@gmail.com>
2022-02-21 11:02:54 +0200

    start on generating docs

--- packages/site/src/views/Api.jsx
+++ packages/site/src/views/Api.jsx
@@ -1,5 +1,44 @@
+import { chdir } from 'process'
 import React from 'react'
+import styled from 'styled-components'
+import apiDescription from '../api.json'
+
+console.log({apiDescription})
+
+const Wrapper = styled.div`
+  max-width: 650px;
+  margin: 0 auto;
+`;
 
 export const Api = () => {
-  return <div>hello from docs</div>
+  return <Wrapper>
+    {
+      apiDescription.children.map((node) => (
+        <div key={node.name}>
+          <b>{node.name}</b>
+          {
+            node.children && 
+            <table>
+                <thead>
+                  <th>name</th>
+                  <th>type</th>
+                  <th>description</th>
+                </thead>
+                {
+                  node.children?.map?.(child => {
+                    return (
+                      <tr>
+                        <td>{child.name}</td>
+                        <td>{child?.type?.name}</td>
+                        <td>{child?.comment?.shortText}</td>
+                      </tr>
+                    )
+                  })
+                }
+            </table>
+          }
+        </div>
+      ))
+    }
+  </Wrapper>
 }
